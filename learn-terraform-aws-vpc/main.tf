f11538a1ae528b02d183c1a90f49f127a7cc44bf
bellabeen <rootsquid@gmail.com>
2024-01-04 16:18:57 +0700

    update assosiate internet split route table

--- learn-terraform-aws-vpc/main.tf
+++ learn-terraform-aws-vpc/main.tf
@@ -21,16 +21,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-03caf91bb3d81b843"
-#   instance_type = "t2.micro"
-#   subnet_id = "subnet-030f2231dd5a1ff22"
-
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
-
 resource "aws_vpc" "main" {
   cidr_block = "20.0.0.0/16"
 
@@ -62,26 +52,42 @@ resource "aws_subnet" "subnet_private" {
 
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main.id
-  tags = {
+  tags   = {
     Name = "default igw"
   }
 }
 
-resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.main.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
- 
- tags = {
-   Name = "2nd Route Table"
- }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  # Define your private route(s) here if needed
+
+  tags = {
+    Name = "Private Route Table"
+  }
 }
 
 resource "aws_route_table_association" "public_subnet_asso" {
- count = length(var.subnet_public_cidr)
- subnet_id      = element(aws_subnet.subnet_public[*].id, count.index)
- route_table_id = aws_route_table.second_rt.id
+  count          = length(var.subnet_public_cidr)
+  subnet_id      = element(aws_subnet.subnet_public[*].id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(var.subnet_private_cidr)
+  subnet_id      = element(aws_subnet.subnet_private[*].id, count.index)
+  route_table_id = aws_route_table.private_rt.id
 }
