84a19d26e7474df9493bca5a7099f30a23d46c36
bellabeen <rootsquid@gmail.com>
2024-01-04 16:04:23 +0700

    update assosiate internet gw on subnet public

--- learn-terraform-aws-vpc/main.tf
+++ learn-terraform-aws-vpc/main.tf
@@ -58,3 +58,30 @@ resource "aws_subnet" "subnet_private" {
     Name = "Private Subnet ${count.index + 1}"
   }
 }
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "default igw"
+  }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gateway.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.subnet_public_cidr)
+ subnet_id      = element(aws_subnet.subnet_public[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
