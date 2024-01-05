71719d4ad6098bfbd011260630bbd4f09dc3e8ef
bellabeen <rootsquid@gmail.com>
2024-01-05 14:05:18 +0700

    Add comment to documentation code

--- learn-terraform-aws-vpc-multiple-subnet/main.tf
+++ learn-terraform-aws-vpc-multiple-subnet/main.tf
@@ -1,12 +1,10 @@
 terraform {
     cloud {
         organization = "kedaiit-org"
-
         workspaces {
         name = "learn-tfc-aws"
         }
     }
-    
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -19,6 +17,7 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
+  # Create tags for the all resource add global tagging
     default_tags {
       tags = {
           "tunas:env" = "prod"
@@ -28,6 +27,7 @@ provider "aws" {
   }
 }
 
+# Create Segment VPC
 resource "aws_vpc" "main" {
   cidr_block = "20.0.0.0/16"
 
@@ -36,6 +36,7 @@ resource "aws_vpc" "main" {
   } 
 }
 
+# Create Public Subnet on 3 Availbility Zone
 resource "aws_subnet" "subnet_public" {
   count = length(var.subnet_public_cidr)
   vpc_id = aws_vpc.main.id
@@ -46,6 +47,7 @@ resource "aws_subnet" "subnet_public" {
   }
 }
 
+# Create Default Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main.id
   tags   = {
@@ -53,6 +55,7 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
+# Integration Subnet Public To Internet Gateway
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -66,12 +69,14 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Loop All Integration Subnet Public To Internet Gateway  
 resource "aws_route_table_association" "public_subnet_asso" {
   count          = length(var.subnet_public_cidr)
   subnet_id      = element(aws_subnet.subnet_public[*].id, count.index)
   route_table_id = aws_route_table.public_rt.id
 }
 
+# Create Private Subnet EC2 On 3 Availbility Zone
 resource "aws_subnet" "subnet_private_ec2" {
   count            = length(var.subnet_private_ec2_cidr)
   vpc_id           = aws_vpc.main.id
@@ -82,6 +87,7 @@ resource "aws_subnet" "subnet_private_ec2" {
   }
 }
 
+# Create Route Table Assosiated Subnet EC2 On 3 Availbility Zone
 resource "aws_route_table" "private_rt" {
   count = length(var.subnet_private_ec2_cidr)
   vpc_id = aws_vpc.main.id
@@ -91,15 +97,18 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
+# Integration Route Table Private EC2 Assosiated Subnet EC2 On 3 Availbility Zone
 resource "aws_route_table_association" "private_subnet_asso" {
   count = length(var.subnet_private_ec2_cidr)
   subnet_id      = aws_subnet.subnet_private_ec2[count.index].id
   route_table_id = aws_route_table.private_rt[count.index].id
 }
 
+# Create NAT Gateway
 resource "aws_eip" "nat_eip" {
 }
 
+# Assosiated Subnet Private EC2 With NAT Gateway
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.subnet_private_ec2[0].id  # Just use the first private subnet for EC2 instances, assuming all private subnets use the same route table
@@ -107,15 +116,12 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "Default NAT Gateway"
     # Add more tags as needed
   }
-
 }
 
 resource "aws_route" "private_subnet_route" {
   count = length(var.subnet_private_ec2_cidr)
-
   route_table_id          = aws_route_table.private_rt[count.index].id
   destination_cidr_block  = "0.0.0.0/0"  # Default route for NAT Gateway
-
   # Specify the ID of the NAT Gateway in the gateway_id attribute
   gateway_id = aws_nat_gateway.nat_gateway.id
 }
@@ -130,7 +136,7 @@ resource "aws_default_route_table" "default_rt" {
   }
 }
 
-#####
+# Create Private Subnet DB On 3 Availbility Zone
 resource "aws_subnet" "subnet_private_db" {
   count            = length(var.subnet_private_db_cidr)
   vpc_id           = aws_vpc.main.id
@@ -141,6 +147,7 @@ resource "aws_subnet" "subnet_private_db" {
   }
 }
 
+# Create Route Table Assosiated Subnet DB On 3 Availbility Zone
 resource "aws_route_table" "private_rt_db" {
   count = length(var.subnet_private_db_cidr)
   vpc_id = aws_vpc.main.id
@@ -150,6 +157,7 @@ resource "aws_route_table" "private_rt_db" {
   }
 }
 
+# Integration Route Table Private DB Assosiated Subnet DB On 3 Availbility Zone
 resource "aws_route_table_association" "private_subnet_db_asso" {
   subnet_id      = aws_subnet.subnet_private_ec2[0].id  # Assuming you have only one private subnet for EC2 instances
   route_table_id = aws_route_table.private_rt[0].id
